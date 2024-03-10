93bf907a615bc068bbb0350f936a4944e55367b1
AndressaGabriele <andressa.chagas79@gmail.com>
2024-03-10 17:35:37 -0300

    Remove console.log used for testing

--- src/utils/helpers/dateFormatter.ts
+++ src/utils/helpers/dateFormatter.ts
@@ -2,6 +2,5 @@ export const formatDate = (timestamp: number): string => {
   const date = new Date(timestamp);
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
-  console.log(timestamp);
   return `${month}/${year}`;
 };
